c4f625ec9a9566befc91a5e22023e6cd62e4dadb
Truong Le <truong.le@bbv.vn>
2021-03-29 16:48:11 +0700

    Truong commit

--- src/app/task-indicator/task-indicator.component.ts
+++ src/app/task-indicator/task-indicator.component.ts
@@ -1,15 +1,54 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, AfterViewInit, ViewChild, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-task-indicator',
   templateUrl: './task-indicator.component.html',
   styleUrls: ['./task-indicator.component.scss']
 })
-export class TaskIndicatorComponent implements OnInit {
+export class TaskIndicatorComponent implements OnInit, AfterViewInit, OnChanges {
   @Input() currentTask: any;
   constructor() { }
 
+
+  @ViewChild('progressContainer') progressContainer;
+  progress;
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    this.progress = [...this.progressContainer.nativeElement.children];
+    this.playNext();
+    this.progress.map(el => {
+      el.addEventListener('animationend', (e) => this.playNext(e));
+    });
+  }
+  ngOnChanges(changes: any) {
+    console.log(changes);
+    
+    if(changes) {
+      this.ngAfterViewInit()
+    }
+    
+  }
+
+  playNext = (e?: any) => {
+    const current = e && e.target;
+    let next;
+    if (current) {
+      const currentIndex = this.progress.indexOf(current);
+      if (currentIndex < this.progress.length) {
+        next = this.progress[currentIndex + 1];
+      }
+      current.classList.remove('active');
+      current.classList.add('passed');
+    }
+    if (!next) {
+      this.progress.map(el => {
+        el.classList.remove('active');
+        el.classList.remove('passed');
+      });
+      next = this.progress[0];
+    }
+    next.classList.add('active');
+  };
 }
