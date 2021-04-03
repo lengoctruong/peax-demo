a42bde6cf19ac0623773d5b1a158eefcd0ba549e
Nguyen Hoa <hnguyen5295@gmail.com>
2021-04-03 10:59:35 +0700

    - Jest setup - Remove Karma and Jasmine

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "preset": "jest-preset-angular",
+  "setupFilesAfterEnv": [
+    "<rootDir>/setup-jest.ts"
+  ],
+  "transform": {
+    "^.+\\.(ts|js|html)$": "ts-jest"
+  },
+  "transformIgnorePatterns": [
+    "node_modules/(?!@ngrx|ngx-socket-io)"
+  ],
+  "testPathIgnorePatterns": [
+    "<rootDir>/node_modules/",
+    "<rootDir>/dist/",
+    "<rootDir>/cypress/",
+    "<rootDir>/test.ts/"
+  ],
+  "globals": {
+    "ts-jest": {
+      "tsConfig": "<rootDir>/tsconfig.spec.json",
+      "stringifyContentPathRegex": "\\.html$"
+    }
+  }
+};
