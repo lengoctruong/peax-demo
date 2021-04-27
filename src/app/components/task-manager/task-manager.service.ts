c7ae63c6d13b2cc587cf6c7e3b09d3a930211dea
hnguyen5295 <hnguyen5295@gmail.com>
2021-04-27 13:54:56 +0700

    [FE] Update Category UI

--- src/app/components/task-manager/task-manager.service.ts
+++ src/app/components/task-manager/task-manager.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 import * as TaskManagerModel from '@components/task-manager/models/task-manager.model';
 
 @Injectable({
