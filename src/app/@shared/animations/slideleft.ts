da4428d3649a6df49e27b0f659f7543eaa520c0b
hnguyen5295 <hnguyen5295@gmail.com>
2021-04-27 10:30:13 +0700

    [FE] Update

--- src/app/@shared/animations/slideleft.ts
+++ src/app/@shared/animations/slideleft.ts
@@ -7,7 +7,6 @@ export const slideLeftLastItem = (
   const eleLength = elements.length;
 
   elements[eleLength - 1].classList.add(cssClass1);
-  elements[eleLength - 1].classList.remove('flex-0');
 
   setTimeout(() => {
     elements[eleLength - 1].classList.add(cssClass2);
